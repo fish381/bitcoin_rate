fa2118f6e0711fd0f773b26f79036945126f53cb
fish381 <79244742+fish381@users.noreply.github.com>
2021-03-18 20:04:32 +1100

    Update code.index.js

--- bitcoin/code.index.js
+++ bitcoin/code.index.js
@@ -1,28 +1,14 @@
 console.log ("starting up API")
-
 var textbox = document.getElementById("textbox");
 var button = document.getElementById("button");
-
-
 button.addEventListener("click", function() {
-     localStorage.messages = textbox.value; 
-    
-    
+     localStorage.messages = textbox.value;   
     });
-
 button.addEventListener("click", function(){
      if (!isNaN) {
           alert("that not a number boy!")
      } else{
      var newMessage = document.createElement("li");
      newMessage.innerHTML = textbox.value;
-     }
-
-     
-     
+     }     
 });
-
-
-
-
-
