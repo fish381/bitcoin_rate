cfd934f11200b3c9c051a68d62487424bb87e723
Thekidwhoategrassatrecces <80889102+Thekidwhoategrassatrecces@users.noreply.github.com>
2021-03-24 17:32:15 +1100

    Bitch ya mums a hoe
    Fuck ya nan like i fucked this code

---
+++ bitcoin_1/bitcoin/code.index.js
@@ -0,0 +1,14 @@
+console.log ("starting up API")
+var textbox = document.getElementById("textbox");
+var button = document.getElementById("button");
+button.addEventListener("click", function() {
+     localStorage.messages = textbox.value;   
+    });
+button.addEventListener("click", function(){
+     if (!isNaN) {
+          alert("that not a number boy!")
+     } else{
+     var newMessage = document.createElement("li");
+     newMessage.innerHTML = textbox.value;
+     }     
+});
