01e8f9049c31c25ab39aa4937e62ae3371020ac7
fish381 <79244742+fish381@users.noreply.github.com>
2021-03-24 19:23:16 +1100

    fixing

--- bitcoin_1/bitcoin/code.index.js
+++ bitcoin_1/bitcoin/code.index.js
@@ -4,11 +4,8 @@ var button = document.getElementById("button");
 button.addEventListener("click", function() {
      localStorage.messages = textbox.value;   
     });
-button.addEventListener("click", function(){
-     if (!isNaN) {
-          alert("that not a number boy!")
-     } else{
-     var newMessage = document.createElement("li");
-     newMessage.innerHTML = textbox.value;
+button.addEventListener("click", function(){    
+          var newMessage = document.createElement("li");
+          newMessage.innerHTML = textbox.value;    
      }     
-});
+);
